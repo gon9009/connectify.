2e6a0e981f9e05618415bec33395c71814219b23
gon <jrteab99@gmail.com>
2025-02-25 16:37:50 +0900

    feat: 모바일 헤더 컴포넌트 제작 완료
    - 모바일 화면에서의 헤더  컴포넌트 구현

---
+++ src/components/layout/MobileHeader.tsx
@@ -0,0 +1,74 @@
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
+import { useUserContext } from "../../context/AuthContext";
+import { useSignOutAccount } from "../../lib/react-query/queries";
+import { INITIAL_USER } from "../../constants/authState";
+
+// 모바일 헤더 로고 ,
+const MobileHeaderLogo = () => {
+  return (
+    <Link to="/">
+      <img
+        src="/assets/logo.png"
+        alt="logo"
+        className="logo mobile-header__logo"
+      />
+    </Link>
+  );
+};
+
+// 모바일 로그아웃 버튼
+const MobileLogoutButton = ({
+  handleSignout,
+}: {
+  handleSignout: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}) => {
+  return (
+    <Button className="mobile-header__logout" onClick={handleSignout}>
+      <img src="/assets/logout.svg" alt="로그아웃" />
+    </Button>
+  );
+};
+
+// 모바일 유저 프로필
+const MobileUserProfile = ({ user }: { user: typeof INITIAL_USER }) => {
+  return (
+    <Link to={`/profile/${user.id}`} className="mobile-header__profile">
+      <img
+        src={user.imageUrl || "/assets/placeholder.svg"}
+        alt="프로필"
+        className="sidebar__profile-img"
+      />
+    </Link>
+  );
+};
+
+// 모바일 헤더
+const MobileHeader = () => {
+  const navigate = useNavigate();
+  const { user, setUser } = useUserContext();
+  const { mutate: signOut } = useSignOutAccount();
+
+  const handleSignout = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    signOut();
+    setUser(INITIAL_USER);
+    navigate("/sign-in");
+  };
+
+  return (
+    <section className="mobile-header">
+      <div className="mobile-header-container">
+        <MobileHeaderLogo />
+        <div className="mobile-hedaer__actions">
+          <MobileLogoutButton handleSignout={handleSignout} />
+          <MobileUserProfile user={user} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MobileHeader;
