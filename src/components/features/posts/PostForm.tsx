4d1ae58f47e1a2b9bc6ed493f3b0f157add26478
gon <jrteab99@gmail.com>
2025-03-02 09:46:55 +0900

    feat: TextArea 공통 컴포넌트 제작
    - PostForm 에서 쓰일 TextArea 컴포넌트 제작

--- src/components/features/posts/PostForm.tsx
+++ src/components/features/posts/PostForm.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import Label from "../../ui/Label";
+import Button from "../../ui/Button";
+import Input from "../../ui/Input";
+import Textarea from "../../ui/Textarea";
 
-const PostForm = () => {
-  return (
-    <div>PostForm</div>
-  )
-}
+// 포스트 폼 필드 
+const PostForm = ({ post, action, onCreate, onUpdate }) => {
 
-export default PostForm
+    const onSubmit = () => {
+        return (
+
+        )
+    }
+
+    return (
+      <form>
+        {/* Caption 필드 */}
+        <div>
+          <Label htmlFor="caption">Caption</Label>
+          <Textarea id="caption" />
+        </div>
+  
+        {/* 파일 업로더 자리 표시 */}
+        <div>
+          <Label>사진 추가하기</Label>
+          <div>파일 업로더 영역</div>
+        </div>
+  
+        {/* Location 필드 */}
+        <div>
+          <Label htmlFor="location">위치 추가하기</Label>
+          <Input id="location" />
+        </div>
+  
+        {/* Tags 필드 */}
+        <div>
+          <Label htmlFor="tags">태그 추가하기기</Label>
+          <Input id="tags" />
+        </div>
+  
+        {/* 기본 버튼 그룹 */}
+        <div>
+          <Button>취소</Button>
+          <Button>제출</Button>
+        </div>
+      </form>
+    );
+  };
